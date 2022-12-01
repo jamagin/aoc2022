c66224f1b013e86ba6c2cb5717daba44c7dc2f8a
Jamie Magin <jamie@lowerarchy.com>
2022-12-01 16:36:22 +0000

    day 1 solved

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,4 +1,4 @@
-use std::cmp;
+use std::collections::BinaryHeap;
 use std::fs::File;
 use std::io::{BufReader, self};
 use std::io::prelude::*;
@@ -8,19 +8,21 @@ fn main() -> io::Result<()> {
     let input_data = Path::new(env!("CARGO_MANIFEST_DIR")).join("data").join("1.txt");
     let fhandle = File::open(input_data)?;
     let br = BufReader::new(fhandle);
-    let mut largest: u64 = 0;
+    let mut totals: BinaryHeap<u64> = BinaryHeap::new();
     let mut current: u64 = 0;
 
     for line in br.lines() {
         let res = line.unwrap().parse::<u64>();
         match res {
             Err(_) => {
-                largest = cmp::max(largest, current);
+                totals.push(current);
                 current = 0;
             },
             Ok(item) => current += item,
         }
     }
-    print!("{}", largest);
+
+    let top = totals.into_sorted_vec();
+    println!("{}", top[top.len() - 3 ..].iter().fold(0, |acc, x| acc + x));
     Ok(())
 }
