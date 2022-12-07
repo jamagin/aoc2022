01b1948498b662313248ae7f8b853979a2baa18f
Jamie Magin <jamie@lowerarchy.com>
2022-12-07 01:44:52 +0000

    Day 6, part 2

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -7,11 +7,19 @@ fn main() -> io::Result<()> {
 
     let input = input_data_to_string("6.txt")?;
     let chars: Vec<char> = input.chars().collect();
+    let mut seen_sop = false;
     for i in (3+4)..chars.len() {
-        let set: HashSet<char> = chars[i-4..i].iter().copied().collect();
-        if set.len() == 4 {
-            println!("{i}");
-            break;
+        let set_4: HashSet<char> = chars[i-4..i].iter().copied().collect();
+        if !seen_sop && set_4.len() == 4 {
+            seen_sop = true;
+            println!("SOP {i}");
+        }
+        if i > 14 {
+            let set_14: HashSet<char> = chars[i-14..i].iter().copied().collect();
+            if set_14.len() == 14 {
+                println!("SOM {i}");
+                break;
+            }
         }
     }
     Ok(())
